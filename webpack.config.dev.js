ec2133cb772bb895f0fe22c7b8b650dc5e3f85a8
Nikita Melnikov <nikmel2803@gmail.com>
2019-07-30 14:45:35 +0300

    fix webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,12 +1,10 @@
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config');
-const path = require('path');
 
 module.exports = merge(baseConfig, {
   mode: 'development',
   devServer: {
     port: 9000,
-    watchContentBase: true,
     open: true,
     openPage: 'test'
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
-const path = require('path');
-
 module.exports = {
   mode: 'production',
   entry: './src/main.js',
   output: {
-    filename: 'hawk.js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'umd'
+    filename: './hawk.js',
+    libraryTarget: 'umd',
+    library: 'hawk'
   }
 };
