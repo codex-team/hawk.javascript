a396e5e74e819f17045896a95fbcee2066202c21
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-22 01:39:12 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
-module.exports = [
-  {
+const merge = require('webpack-merge');
+
+/**
+ * Returns base config or its modification if mergeObject passed
+ * @param {Object} mergeObject - object for modification base configuration (will be merged with base config)
+ * @return {Object}
+ */
+function getBaseConfig(mergeObject = {}) {
+  const baseConfig = {
     mode: 'production',
     entry: './src/main.js',
     output: {
@@ -14,22 +21,17 @@ module.exports = [
         }
       ]
     }
-  },
-  {
-    mode: 'production',
-    entry: './src/main.js',
+  };
+
+  return merge(baseConfig, mergeObject);
+}
+
+module.exports = [
+  getBaseConfig(),
+  getBaseConfig({
     output: {
-      filename: './hawk.umd.js',
-      library: 'HawkCatcher',
+      filename: 'hawk.umd.js',
       libraryTarget: 'umd'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js/,
-          use: 'babel-loader'
-        }
-      ]
     }
-  }
+  })
 ];
