68637f265c857657159014f5f19bbebb444fe197
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-22 00:09:01 +0300

    add catcher umd library

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
-module.exports = {
-  mode: 'production',
-  entry: './src/main.js',
-  output: {
-    filename: './hawk.js',
-    library: 'HawkCatcher'
+module.exports = [
+  {
+    mode: 'production',
+    entry: './src/main.js',
+    output: {
+      filename: './hawk.js',
+      library: 'HawkCatcher'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js/,
+          use: 'babel-loader'
+        }
+      ]
+    }
   },
-  module: {
-    rules: [
-      {
-        test: /\.js/,
-        use: 'babel-loader'
-      }
-    ]
+  {
+    mode: 'production',
+    entry: './src/main.js',
+    output: {
+      filename: './hawk.umd.js',
+      library: 'HawkCatcher',
+      libraryTarget: 'umd'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js/,
+          use: 'babel-loader'
+        }
+      ]
+    }
   }
-};
+];
