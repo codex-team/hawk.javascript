de06327b4e241e9e9a86ea150b9a9c55677f85ba
Taly <vitalik7tv@yandex.ru>
2020-11-26 20:51:45 +0300

    delete dist folder

--- dist/addons/consoleCatcher.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @file Integration for catching console logs and other info
- */
-/**
- * @param event - event to modify
- * @param data - event data
- */
-export default function (event: any, data: any): void;
--- dist/modules/fetchTimer.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Sends AJAX request and wait for some time.
- * If time is exceeded, cancel the request.
- *
- * @param {string} url — request endpoint
- * @param {number} ms — maximum request time allowed
- */
-export default function fetchTimer(url: string, ms: number): Promise<any>;
