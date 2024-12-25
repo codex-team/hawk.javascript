5791c695a2b6e6b11bf540884fcc51ef202e6dd7
GeekaN2 <geekan@bk.ru>
2024-12-25 20:03:38 +0300

    lint: add comments

--- src/modules/storage.ts
+++ src/modules/storage.ts
@@ -5,25 +5,26 @@
 class CatcherStorage implements Storage {
   private items: { [key: string]: string } = {};
 
-  private isLocalStorageAvailable(): boolean {
-    try {
-      const testKey = '__test__';
-      localStorage.setItem(testKey, testKey);
-      localStorage.removeItem(testKey);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-
-  getItem(key: string): string | null {
+  /**
+   * Returns the value of the item with the specified key.
+   *
+   * @param key - The key of the item you want to retrieve
+   */
+  public getItem(key: string): string | null {
     if (this.isLocalStorageAvailable()) {
       return localStorage.getItem(key);
     }
+
     return this.items[key] || null;
   }
 
-  setItem(key: string, value: string): void {
+  /**
+   * Sets the value of the pair identified by key to value, creating a new key/value pair if none existed for key previously.
+   *
+   * @param key - The key of the item you want to store
+   * @param value - The value of the item you want to store
+   */
+  public setItem(key: string, value: string): void {
     if (this.isLocalStorageAvailable()) {
       localStorage.setItem(key, value);
     } else {
@@ -31,7 +32,12 @@ class CatcherStorage implements Storage {
     }
   }
 
-  removeItem(key: string): void {
+  /**
+   * Removes the key/value pair with the given key from the list associated with the object, if a key/value pair with the given key exists.
+   *
+   * @param key - The key of the item you want to remove
+   */
+  public removeItem(key: string): void {
     if (this.isLocalStorageAvailable()) {
       localStorage.removeItem(key);
     } else {
@@ -39,7 +45,10 @@ class CatcherStorage implements Storage {
     }
   }
 
-  clear(): void {
+  /**
+   * Empties the list associated with the object of all key/value pairs, if there are any.
+   */
+  public clear(): void {
     if (this.isLocalStorageAvailable()) {
       localStorage.clear();
     } else {
@@ -47,7 +56,10 @@ class CatcherStorage implements Storage {
     }
   }
 
-  get length(): number {
+  /**
+   * Returns the number of key/value pairs currently present in the list associated with the object.
+   */
+  public get length(): number {
     if (this.isLocalStorageAvailable()) {
       return localStorage.length;
     }
@@ -55,11 +67,33 @@ class CatcherStorage implements Storage {
     return Object.keys(this.items).length;
   }
 
-  key(index: number): string | null {
+  /**
+   * Returns the name of the nth key in the list.
+   *
+   * @param index - The index of the key you want to get
+   */
+  public key(index: number): string | null {
     if (this.isLocalStorageAvailable()) {
       return localStorage.key(index);
     }
+
     return Object.keys(this.items)[index] || null;
+  }
+
+  /**
+   * Checks if localStorage is available
+   */
+  private isLocalStorageAvailable(): boolean {
+    try {
+      const testKey = '__test__';
+
+      localStorage.setItem(testKey, testKey);
+      localStorage.removeItem(testKey);
+
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
 
