224bf495cdbe1576e419bf3effb9b0e5f0a3f253
e11sy <130844513+e11sy@users.noreply.github.com>
2025-03-08 16:18:28 +0300

    imp(source-maps): do not send minified source cod frames

--- src/modules/stackParser.ts
+++ src/modules/stackParser.ts
@@ -70,14 +70,28 @@ export default class StackParser {
       const linesCollectCount = 5;
       const lineFrom = Math.max(0, actualLineNumber - linesCollectCount);
       const lineTo = Math.min(lines.length - 1, actualLineNumber + linesCollectCount + 1);
-      const linesToCollect = lines.slice(lineFrom, lineTo);
 
-      return linesToCollect.map((content, index) => {
-        return {
-          line: lineFrom + index + 1,
-          content,
-        };
-      });
+      const sourceCodeLines: SourceCodeLine[] = [];
+      let extractedLineIndex = 1;
+
+      /**
+       * In some cases column number of the error stack trace frame would be less then 200, but source code is minified
+       * For this cases we need to check, that all of the lines to collect have length less than 200 too
+       */
+      for (const lineToCheck in lines.slice(lineFrom, lineTo)) {
+        if (lineToCheck.length > 200) {
+          return null;
+        } else {
+          sourceCodeLines.push({
+            line: lineFrom + extractedLineIndex,
+            content: lineToCheck,
+          });
+
+          extractedLineIndex += 1;
+        }
+      }
+
+      return sourceCodeLines;
     } catch (e) {
       console.warn('Hawk JS SDK: Can not extract source code. Please, report this issue: https://github.com/codex-team/hawk.javascript/issues/new', e);
 
